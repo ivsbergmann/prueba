f4f14574bc3f9d541b61631fb5517d0cb5daba56
Ivanna <tiarebrg@gmail.com>
2019-05-15 16:43:32 -0300

    se modificaron todas las carpetas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,38 @@
-$(document).ready(function(){
-console.log("En jquery");
-$("#supermama").click(function(){
-    console.log("click en icono");
-    $("#modalsupermama").modal('show');
-    
-})
-    
-$("#cuentacuentos").click(function(){
-    console.log("click en icono");
-    $("#modalcuentacuentos").modal('show');
-    
-})
+$(document).ready(function () {
+    console.log("En jquery");
+    $("#supermama").click(function () {
+        console.log("click en icono");
+        $("#modalsupermama").modal('show');
 
-$("#dev-front").click(function(){
-    console.log("click en icono");
-    $("#modaldev-front").modal('show');
-    
-})
+    })
 
-$('[tool-tip-toggle="tooltip-demo"]').tooltip({
-  placement : 'top'
- 
+    $("#cuentacuentos").click(function () {
+        console.log("click en icono");
+        $("#modalcuentacuentos").modal('show');
+
+    })
+
+    $("#dev-front").click(function () {
+        console.log("click en icono");
+        $("#modaldev-front").modal('show');
+
+    })
+
+    $('[tool-tip-toggle="tooltip-demo"]').tooltip({
+        placement: 'top'
+
     });
- 
+
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault()
+
+        $('html, body').animate(
+            {
+                scrollTop: $($(this).attr('href')).offset().top,
+            },
+            500,
+            'linear'
+        )
+    });
 
 });

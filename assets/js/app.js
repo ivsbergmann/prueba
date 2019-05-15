264fb0658c19dca83d78f03c8dc1e891b1cfb5b4
Ivanna <tiarebrg@gmail.com>
2019-05-15 09:24:56 -0300

    se agregan modales

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+console.log("En jquery");
+$("#supermama").click(function(){
+    console.log("click en icono");
+    $("#modalsupermama").modal('show');
+    
+})
+    
+$("#cuentacuentos").click(function(){
+    console.log("click en icono");
+    $("#modalcuentacuentos").modal('show');
+    
+})
+
+$("#dev-front").click(function(){
+    console.log("click en icono");
+    $("#modaldev-front").modal('show');
+    
+})
+})
